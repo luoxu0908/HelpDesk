6185509eb32ddbd0124122a5cbd6b34550093901
ningguo <ningguo.an@bizcube.com.sg>
2018-09-13 16:40:35 +0800

    update

--- src/assets/js/lib/bizcube-api.js
+++ src/assets/js/lib/bizcube-api.js
@@ -3,7 +3,7 @@ class BizcubeAPi {
       //this.url = 'https://portal.taksys.com.sg/Support/';
   }
   static getURL() {
-    return 'https://portal.taksys.com.sg/BCSupport/';
+    return 'https://portal.taksys.com.sg/HelpDesk/';
   }
 
 }
